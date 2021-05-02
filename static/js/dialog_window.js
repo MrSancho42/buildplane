524324d15ce014acdc9c555bd30a431d50c6076b
s0fko <faberdasss@gmail.com>
2021-05-02 20:15:31 +0300

    Add dialog window (dont work right)

---
+++ static/js/dialog_window.js
@@ -0,0 +1,19 @@
+(function() {
+    let dialogButton = document.getElementsByClassName('form-settings__delete-button')[0];
+    let dialogWindow = document.getElementsByClassName('dialog-box')[0];
+
+    dialogButton.addEventListener('click', () => {
+        dialogWindow.classList.add('dialog-box__active')
+        dialogWindow.showModal();
+        let greenButton = document.getElementById('dialog-box__green-button')[0];
+        let redButton = document.getElementById('dialog-box__red-button')[0];
+    })
+
+    greenButton.addEventListener('click', () => {
+        dialogWindow.classList.remove('dialog-box__active')
+    })
+
+    redButton.addEventListener('click', () => {
+        dialogWindow.classList.remove('dialog-box__active')
+    })
+})();
