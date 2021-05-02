857405167a6623f0a325b0141b87cc918b864041
s0fko <faberdasss@gmail.com>
2021-05-02 20:44:29 +0300

    Fix js variables

--- static/js/dialog_window.js
+++ static/js/dialog_window.js
@@ -5,15 +5,16 @@
     dialogButton.addEventListener('click', () => {
         dialogWindow.classList.add('dialog-box__active')
         dialogWindow.showModal();
-        let greenButton = document.getElementById('dialog-box__green-button')[0];
-        let redButton = document.getElementById('dialog-box__red-button')[0];
-    })
 
-    greenButton.addEventListener('click', () => {
-        dialogWindow.classList.remove('dialog-box__active')
-    })
-
-    redButton.addEventListener('click', () => {
-        dialogWindow.classList.remove('dialog-box__active')
+        let greenButton = document.getElementsByClassName('dialog-box__green-button')[0];
+        let redButton = document.getElementsByClassName('dialog-box__red-button')[0];
+    
+        greenButton.addEventListener('click', () => {
+            dialogWindow.classList.remove('dialog-box__active')
+        })
+    
+        redButton.addEventListener('click', () => {
+            dialogWindow.classList.remove('dialog-box__active')
+        })
     })
 })();
