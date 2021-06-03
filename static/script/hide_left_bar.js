f210ffc6c69032a457fc952b7414f59e1af75397
MrSancho42 <sashavoznuy@gmail.com>
2021-06-04 00:26:01 +0300

    new hidden left bar

---
+++ static/script/hide_left_bar.js
@@ -0,0 +1,19 @@
+function hide_left_bar(button) {
+	const left_bar = document.getElementsByClassName('left-bar')[0];
+	const work_area__bar = document.getElementsByClassName('work-area__bar')[0];
+
+	if (button.dataset.status === 'true') {
+		left_bar.classList.add('left-bar_hidden');
+		button.classList.add('left-bar__minimize_hidden');
+		work_area__bar.classList.add('work-area__bar_hidden');
+
+		button.dataset.status = 'false';
+	}
+	else {
+		left_bar.classList.remove('left-bar_hidden');
+		button.classList.remove('left-bar__minimize_hidden');
+		work_area__bar.classList.remove('work-area__bar_hidden');
+
+		button.dataset.status = 'true';
+	}
+}
